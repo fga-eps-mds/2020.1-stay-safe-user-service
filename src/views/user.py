3731196c8aa2f87c53db17bcd5c957ebab46b43b
Sara Silva <saracsas2@gmail.com>
2020-09-05 21:01:14 -0300

    Add header validation

--- src/views/user.py
+++ src/views/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from flask_cors import CORS
+from functools import wraps
 
 from controllers import user as controller
 from utils.formatters import create_response
@@ -8,7 +9,30 @@ user_blueprint = Blueprint('user', __name__, url_prefix='/api')
 CORS(user_blueprint)
 
 
+def validate_header(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        TYPE_ERROR = ('Unsupported media type', 415)
+        header_values = ['*/*', 'application/json']
+        header_keys = ['Accept', 'Content-Type']
+        header = request.headers
+        rm = request.method
+
+        if rm == 'POST' or rm == 'PUT' or rm == 'PATCH':
+            if not request.data:
+                return create_response(*TYPE_ERROR)
+            elif not all(h in header for h in header_keys):
+                return create_response(*TYPE_ERROR)
+            elif not all(header[h] in header_values for h in header_keys):
+                return create_response(*TYPE_ERROR)
+
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+
 @user_blueprint.route('/users/', methods=['GET', 'POST'])
+@validate_header
 def get_post_rubric():
     if request.method == 'GET':
         response, status = controller.get_all_users()
@@ -21,6 +45,7 @@ def get_post_rubric():
 
 @user_blueprint.route('/users/<string:user_username>',
                       methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+@validate_header
 def user_by_username(user_username):
     if request.method == 'GET':
         response, status = controller.get_one_user(user_username)
