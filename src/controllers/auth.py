445ac4ef5546de4c839ab55ee673e44841b35f9c
Brenda <brendasantosv.bs@gmail.com>
2020-09-10 20:54:38 -0300

    Creating auth function

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -6,3 +6,10 @@ def generateRandomString(tamanho):
     key = ''.join(random.choice(random_str)) for i in range(tamanho)
     return key
 
+def auth(authorization):
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return jsonify ({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth=' 'Login required'}), 401
+
+    return jsonify({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth="Login required"'}), 401
+
