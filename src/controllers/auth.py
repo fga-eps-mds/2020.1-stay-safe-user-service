1cf0a8c390c5653e73dc71408e3deee704b5e0f5
Brenda <brendasantosv.bs@gmail.com>
2020-09-10 21:02:50 -0300

    Changing returns

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -1,5 +1,6 @@
 import string
 import random
+from  utils.formatters import create_response
 
 def generateRandomString(tamanho):
     random_str = string.ascii_letters + string.digits + string.ascii_uppercase
@@ -9,7 +10,18 @@ def generateRandomString(tamanho):
 def auth(authorization):
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
-        return jsonify ({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth=' 'Login required'}), 401
+        return create_response({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth=' 'Login required'}, 401)
 
-    return jsonify({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth="Login required"'}), 401
+    user, _ = get_one_user(auth.username)
+
+    if not user:
+        return create_response({'message': 'user not found', 'data': {}}, 401)
+    
+    if user and check_password_hash(user.password, auth.password):
+        token = jwt.encode({'username': user.name, 'exp': datetime.datetime.now() + datetime.timedelta(hours=12) },
+        app.config['key'])
+        return create_response({'message': 'Validated successfully', 'token': token.decode('UTF-8'),
+                'exp': datetime.datetime.now() + datetime.timedelta(hours = 12)})
+
+    return create_response({'message': 'could not verify', 'WWW-Authenticate': 'Basic auth="Login required"'}, 401)
 
