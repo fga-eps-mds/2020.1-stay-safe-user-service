c50a04a6ac9af93f24fa7e15907df3d2d5114ad3
tsrrodrigues <tsrrodrigues@outlook.com>
2020-10-22 19:30:54 -0300

    Formating json and creating script to save on db.
    Co-authored-by: Brenda <brendasantosv.bs@gmail.com>

--- src/static/save_on_db.py
+++ src/static/save_on_db.py
@@ -1,34 +1,35 @@
 import json
 import requests
 
-with open("./sp2.json", "r") as read_file:
+with open("./neighborhoods.json", "r") as read_file:
     data = json.load(read_file)
-
-data = list(filter(lambda x: x['Uf'] == "SP", data['data']))
 
 headers = {
     "Accept": "*/*",
     "Content-Type": "application/json"
 }
 
-for i in data:
-    c = i['Nome'].split(" - ")
-    if (len(c) == 2):
-        city = c[1]
-        neighborhood = c[0]
-    if (len(c) == 3):
-        city = c[2]
-        neighborhood = c[0] + '-' + c[1]
-    if (len(c) == 4):
-        city = c[3]
-        neighborhood = c[0] + '-' + c[1] + '-' + c[2]
-    m = {
-        "neighborhood": neighborhood,
-        "city": city,
-        "state": i['Uf']
-    }
-    r = requests.post("http://0.0.0.0:8083/api/neighborhood/", data=json.dumps(m), headers=headers)
+errors = []
+
+for neigh in data['DF']:
+    r = requests.post("http://0.0.0.0:8083/api/neighborhood/", data=json.dumps(neigh), headers=headers)
     if(r.status_code != 201):
         print(r.status_code)
-        print(neighborhood)
+        print(neigh['neighborhood'])
+        print()
+        errors.append(neigh)
+
+for neigh in data['SP']:
+    r = requests.post("http://0.0.0.0:8083/api/neighborhood/", data=json.dumps(neigh), headers=headers)
+    if(r.status_code != 201):
+        print(r.status_code)
+        print(neigh['neighborhood'])
+        print()
+        errors.append(neigh)
+
+for i in errors:
+    r = requests.post("http://0.0.0.0:8083/api/neighborhood/", data=json.dumps(neigh), headers=headers)
+    if(r.status_code != 201):
+        print(r.status_code)
+        print(neigh['neighborhood'])
         print()
