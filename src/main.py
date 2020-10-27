ad57d5b10d60773fcd86234615b19cedbc1a2db4
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-27 11:57:25 -0300

    Add the docs blueprint to api
    Co-authored-by: Hérick Portugues <herick.portugues@gamil.com>

--- src/main.py
+++ src/main.py
@@ -25,6 +25,10 @@ app.register_blueprint(auth_blueprint)
 app.register_blueprint(neighborhood_blueprint)
 app.register_blueprint(rating_blueprint)
 app.register_blueprint(favorite_places_blueprint)
+app.register_blueprint(swagger_blueprint, url_prefix='/api/docs')
+
+CORS(app, automatic_options=True)
+
 if __name__ == '__main__':
     server_config = load_configuration()
     logger.info(f"Service configurations: {server_config}")
