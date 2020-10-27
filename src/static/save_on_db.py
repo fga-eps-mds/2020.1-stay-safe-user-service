565818e41bf9e949da7dfe2e513cddd03c160cdf
Rossicler Júnior <rossiclerjr@gmail.com>
2020-10-27 14:51:10 -0300

    Removed duplicated code

--- src/static/save_on_db.py
+++ src/static/save_on_db.py
@@ -11,27 +11,17 @@ headers = {
 
 errors = []
 
-for neigh in data['DF']:
-    r = requests.post("http://0.0.0.0:8083/api/neighborhood/",
-                      data=json.dumps(neigh),
-                      headers=headers
-                      )
-    if(r.status_code != 201):
-        print(r.status_code)
-        print(neigh['neighborhood'])
-        print()
-        errors.append(neigh)
-
-for neigh in data['SP']:
-    r = requests.post("http://0.0.0.0:8083/api/neighborhood/",
-                      data=json.dumps(neigh),
-                      headers=headers
-                      )
-    if(r.status_code != 201):
-        print(r.status_code)
-        print(neigh['neighborhood'])
-        print()
-        errors.append(neigh)
+for state in ['DF', 'SP']:
+    for neigh in data[state]:
+        r = requests.post("http://0.0.0.0:8083/api/neighborhood/",
+                        data=json.dumps(neigh),
+                        headers=headers
+                        )
+        if(r.status_code != 201):
+            print(r.status_code)
+            print(neigh['neighborhood'])
+            print()
+            errors.append(neigh)
 
 for i in errors:
     r = requests.post("http://0.0.0.0:8083/api/neighborhood/",
