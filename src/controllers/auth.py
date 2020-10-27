3297ce4cda376487a4d72219b2484a91b917ffc9
Rossicler Júnior <rossiclerjr@gmail.com>
2020-10-27 15:43:07 -0300

    Refactored authentication method to simplify the code

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -5,30 +5,33 @@ from controllers.user import get_one_user
 
 
 def authentication(auth=None):
-    status = 401
-    response = 'Invalid password'
     if not auth:
+        status = 401
         response = "Login is required"
     elif not auth['username'] or not auth['password']:
+        status = 401
         response = "Username and password are required"
     else:
         result, status_ = get_one_user(auth['username'])
         if status_ == 200:
-            user = result
-            if BCRYPT.check_password_hash(user['password'], auth['password']):
-                token = jwt.encode(
-                    {
-                        'username': user['username'],
-                    },
-                    SECRET_KEY,
-                    algorithm='HS256'
-                )
-                status = 200
-                response = {
-                    'msg': 'Validated successfully',
-                    'token': token.decode('utf-8'),
-                }
+            response, status = get_auth_response(result, auth)
         else:
             response = result
             status = status_
     return response, status
+
+
+def get_auth_response(user, auth):
+    if BCRYPT.check_password_hash(user['password'], auth['password']):
+        token = jwt.encode(
+            {
+                'username': user['username'],
+            },
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+        return {
+            'msg': 'Validated successfully',
+            'token': token.decode('utf-8'),
+        }, 200
+    return 'Invalid password', 401
